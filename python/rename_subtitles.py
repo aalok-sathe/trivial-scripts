f2cb9fba1f1aaf430190c277a23044e21803b74e
Aalok Sathe <aalok.sathe@gmail.com>
2017-01-14 22:09:50 +0530

    Update rename_subtitles.py

--- python/rename_subtitles.py
+++ python/rename_subtitles.py
@@ -1,4 +1,6 @@
 import os
+init = "Rename Subtitles. Copyright (C) 2017 Aalok S.\nThis program comes with ABSOLUTELY NO WARRANTY; for details see GNU GPLv3.\nThis is free software, and you are welcome to redistribute it under certain conditions."
+print(init)
 
 dirs = os.listdir()
 list1=[]
@@ -7,8 +9,8 @@ list2=[]
 for item in dirs:
 	if item[0] != ".":
 		l=len(item)
-		if item[(l)-3:(l)] == "mkv": list1.append(item[:l-4])
-		elif item[(l)-3:(l)] == "srt": list2.append(item[:l-4])
+		if item[(l)-3:(l)] in ["mkv","mp4","avi","mpeg","mov"]: list1.append(item[:l-4])
+		elif item[(l)-3:(l)] in ["srt","sub"]: list2.append(item[:l-4])
 		
 print(list1,list2)
 cwd=os.getcwd()
